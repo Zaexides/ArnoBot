e4354d48a0f5e1bdf374abe8ffa71081860c4716
Zaexides <JeroenEndenburg@hotmail.com>
2019-11-16 14:42:08 +0100

    First setup of response class

--- source/ArnoBot/Core/Response.cs
+++ source/ArnoBot/Core/Response.cs
@@ -6,5 +6,18 @@ namespace ArnoBot.Core
 {
     public class Response
     {
+        public Response.Type ResponseType { get; }
+
+        public Response(Response.Type type)
+        {
+            this.ResponseType = type;
+        }
+
+        public enum Type : uint
+        {
+            Unknown         =0x000_0000_0,
+
+            Executed        =0x001_0001_0,
+        }
     }
 }
