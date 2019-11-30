fe1c7ce1127f1f2c0663c0ebb113df8bc2baffc8
Zaexides <JeroenEndenburg@hotmail.com>
2019-11-30 17:54:13 +0100

    Created and implemented exception responses

---
+++ source/ArnoBot/Core/Exceptions/CommandNotFoundException.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArnoBot.Core.Exceptions
+{
+
+    [Serializable]
+    class CommandNotFoundException : Exception
+    {
+        public CommandNotFoundException() { }
+        public CommandNotFoundException(string message) : base(message) { }
+        public CommandNotFoundException(string message, Exception inner) : base(message, inner) { }
+        protected CommandNotFoundException(
+          System.Runtime.Serialization.SerializationInfo info,
+          System.Runtime.Serialization.StreamingContext context) : base(info, context) { }
+    }
+}
--- source/ArnoBot/Core/Responses/Response.cs
+++ source/ArnoBot/Core/Responses/Response.cs
@@ -19,6 +19,7 @@ namespace ArnoBot.Core.Responses
     public abstract class Response
     {
         public string TextBody { get => ((TextResponse)this).Body; }
+        public Exception Exception { get => ((ErrorResponse)this).Body; }
 
         public enum Type : uint
         {
@@ -27,6 +28,7 @@ namespace ArnoBot.Core.Responses
             Executed = 0x001_0001_0,
 
             NotFound = 0x002_0001_0,
+            Error = 0x002_0002_0,
         }
     }
 }
