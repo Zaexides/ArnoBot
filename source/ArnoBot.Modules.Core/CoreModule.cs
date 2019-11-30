09ed426c65f9a7663189ac33d11d177c75d919e9
Zaexides <JeroenEndenburg@hotmail.com>
2019-11-30 17:19:14 +0100

    Basic core module setup

---
+++ source/ArnoBot.Modules.Core/CoreModule.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using ArnoBot.Interface;
+
+namespace ArnoBot.Modules.Core
+{
+    public class CoreModule : IModule
+    {
+        private Dictionary<string, ICommand> commandRegistry;
+
+        public string Name => "Core";
+
+        public IReadOnlyDictionary<string, ICommand> CommandRegistry => commandRegistry;
+
+        public CoreModule()
+        {
+
+        }
+    }
+}
