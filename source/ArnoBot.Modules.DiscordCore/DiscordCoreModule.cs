2a1c5d54d4782f2e7cfd2123bbfc473df01a546b
Zaexides <JeroenEndenburg@hotmail.com>
2020-01-02 14:35:20 +0100

    Implemented invite link command

---
+++ source/ArnoBot.Modules.DiscordCore/DiscordCoreModule.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using ArnoBot.DiscordBot.Interface;
+using ArnoBot.Interface;
+
+namespace ArnoBot.Modules.DiscordCore
+{
+    public class DiscordCoreModule : IDiscordModule
+    {
+        private Dictionary<string, ICommand> commandRegistry = new Dictionary<string, ICommand>();
+
+        public string Name => "Discord";
+
+        public IReadOnlyDictionary<string, ICommand> CommandRegistry => commandRegistry;
+
+        
+    }
+}
